4a595af44a76bd74e248e23f1956b2a88b4fb475
Dan <idandrei94@gmail.com>
2018-10-23 14:37:25 +0200

    initial commit

---
+++ ParallelProgramming/Exercises/ex1-2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <time.h>
+
+#define CACHE_SIZE 10000
+
+unsigned long long fibonacci_seq(int n) 
+{
+	switch(n)
+	{
+		case 0: 
+			return 0;
+		case 1:
+			return 1;
+		default:
+			;
+			unsigned long long fib1 = 1;
+			unsigned long long fib2 = 1;
+			unsigned long long tmp = 0;
+			for(int i = 2; i < n; ++i)
+			{
+				tmp = fib1 + fib2;
+				fib1 = fib2;
+				fib2 = tmp;
+			}
+			return fib2;
+			
+	}
+}
+
+unsigned long long fibonacci_list[CACHE_SIZE];
+
+unsigned long long fibonacci_rec(int n)
+{
+	switch(n)
+	{
+		case 0: 
+			return 0;
+		case 1:
+			return 1;
+		default:
+		;
+			unsigned long long fib1, fib2;
+			if(n-2 >= CACHE_SIZE || fibonacci_list[n-2] == 0) 
+			{
+				fib1 = fibonacci_rec(n-2);
+				if(n-2 < CACHE_SIZE)
+					fibonacci_list[n-2] = fib1;
+			}
+			else
+				fib1 = fibonacci_list[n-2];
+			if(n-1 >= CACHE_SIZE || fibonacci_list[n-1] == 0) 
+			{
+				fib2 = fibonacci_rec(n-1);
+				if(n-1 < CACHE_SIZE)
+					fibonacci_list[n-1] = fib2;
+			}
+			else
+				fib2 = fibonacci_list[n-1];
+			unsigned long long res = fib1+fib2;
+			fibonacci_list[n] = res;
+			return res;
+			
+	}
+}
+
+int main(void)
+{
+	int n = 46;
+	unsigned long long fib;
+	printf("Algorithm\t\tN value\t\tResult\t\t\tCPU time\tReal time\n");
+	printf("-----------------------------------------------------------------------------------------\n");
+	time_t start_time = time(NULL);
+	clock_t start_clock = clock();
+	fib = fibonacci_seq(n);++fib;--fib;
+	printf("Sequential\t\t%d\t\t%d\t\t%d\t\t%d\n", n, fib, clock()-start_clock, time(NULL)-start_time);
+	start_time = time(NULL);
+	start_clock = clock();
+	fib = fibonacci_rec(n);++fib;--fib;
+	printf("Recurrent\t\t%d\t\t%d\t\t%d\t\t%d\n", n, fib, clock()-start_clock, time(NULL)-start_time);
+}
---
+++ ParallelProgramming/Exercises/marriage.py
@@ -0,0 +1,65 @@
+import random
+import itertools
+import math
+import numpy
+SIZE = 500
+
+def init_data():
+    men = []
+    women = []
+    for _ in range(SIZE):
+        a = numpy.random.permutation(list(range(SIZE)))
+        men.append(list(a))
+    for _ in range(SIZE):
+        a = numpy.random.permutation(list(range(SIZE)))
+        women.append(list(a))
+
+    m_status = []
+    w_status = []
+    for _ in range(SIZE):
+        m_status.append(-1)
+        w_status.append(-1)
+    return men, women, m_status, w_status
+
+def find_wife(man_idx, m_status, w_status, m_pref, w_pref):
+    print("Man #", man_idx, "looking for wife")
+    for i in range(SIZE-1, -1, -1):                         # iterate through each woman by preference until engaged
+        choice = m_pref[man_idx].index(i)                   # asking this woman
+        print("  Asking out woman #", choice)
+        if w_status[choice] == -1:                          # she's available
+            w_status[choice] = man_idx                      # engage woman to man
+            m_status[man_idx] = choice                      # engage man to woman
+            return
+        elif w_pref[man_idx] > w_pref[w_status[choice]]:    # she likes this one more
+            m_status[w_status[choice]] = -1                 # other man cri like baby
+            w_status[choice] = man_idx                      # engage woman to man
+            m_status[man_idx] = choice                      # engage man to woman
+            return
+
+def check(m_status, w_status, m_pref, w_pref):
+    for i in range(SIZE):
+        if w_status[m_status[i]] != i:
+            return False 
+        for j in range(SIZE):
+            if i != j:                  # M1W1 M2W2   
+                M1 = i
+                M2 = j
+                W1 = m_status[i]
+                W2 = m_status[j]
+                if w_pref[W2][M1] > w_pref[W2][M2] and m_pref[M1][W2] > m_pref[M1][W1]:
+                    return False
+                if w_pref[W1][M2] > w_pref[W1][M1] and m_pref[M2][W1] > m_pref[M2][W2]:
+                    return False
+    return True
+
+m_pref, w_pref, m_status, w_status = init_data()
+
+flag = True
+while(flag):
+    flag = False
+    for i in range(SIZE):
+        if m_status[i] == -1:                   # man lf woman
+            flag = True                         # we stop when all men are engaged
+            find_wife(i, m_status, w_status, m_pref, w_pref)
+print("Calculation complete, result is", "correct" if check(m_status, w_status, m_pref, w_pref) else "incorrect")
+
